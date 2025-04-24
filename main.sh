2e5cbc57848a88fd2f9d037d026538f72a8cb3fc
Noah Syrkis <noah@syrkis.com>
2025-04-24 17:57:20 +0200

    Sync archive to S3

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Starting sync..."
+aws s3 sync ~/desk/archive s3://syrkis/ \
+    --endpoint-url https://nbg1.your-objectstorage.com \
+    --exclude main.sh \
+    --exclude .git \
+    --exclude .gitignore \
+    --exclude README.md
+
+echo "Starting git operations..."
+git add .
+git commit -m "Sync archive to S3"
+git push
