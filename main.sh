7332a7158b1f62977b366b31750a65dec38308f0
Noah Syrkis <noah@syrkis.com>
2025-05-07 18:08:38 +0200

    Sync archive to S3

--- main.sh
+++ main.sh
@@ -4,11 +4,11 @@
 echo "Starting sync..."
 aws s3 sync ~/desk/archive s3://syrkis/ \
     --endpoint-url https://nbg1.your-objectstorage.com \
-    --exclude main.sh \
-    --exclude .git/* \
-    --exclude .gitignore \
-    --exclude .DS_Store \
-    --exclude README.md
+    --exclude 'main.sh' \
+    --exclude '.git/*' \
+    --exclude '.gitignore' \
+    --exclude '.DS_Store' \
+    --exclude 'README.md'
 
 echo "Starting git operations..."
 git add .
